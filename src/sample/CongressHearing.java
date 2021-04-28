15b4fc85fdf2239aa36ab3c4eec7318dba898ba4
James Z. Logan Jr <logan4dog@gmail.com>
2021-04-27 20:40:55 -0400

    Version 1.3

---
+++ src/sample/CongressHearing.java
@@ -0,0 +1,49 @@
+package sample;
+
+public class CongressHearing {
+
+  private String congressBody;
+  private String hearingTitle;
+  private String hearingLocation;
+  private String hearingDate;
+
+
+  public CongressHearing(String congressBody, String hearingTitle, String hearingLocation, String hearingDate) {
+    this.congressBody = congressBody;
+    this.hearingTitle = hearingTitle;
+    this.hearingLocation = hearingLocation;
+    this.hearingDate = hearingDate;
+  }
+
+  public String getCongressBody() {
+    return congressBody;
+  }
+
+  public void setCongressBody(String congressBody) {
+    this.congressBody = congressBody;
+  }
+
+  public String getHearingTitle() {
+    return hearingTitle;
+  }
+
+  public void setHearingTitle(String hearingTitle) {
+    this.hearingTitle = hearingTitle;
+  }
+
+  public String getHearingLocation() {
+    return hearingLocation;
+  }
+
+  public void setHearingLocation(String hearingLocation) {
+    this.hearingLocation = hearingLocation;
+  }
+
+  public String getHearingDate() {
+    return hearingLocation;
+  }
+
+  public void setHearingDate(String hearingLocation) {
+    this.hearingLocation = hearingLocation;
+  }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
@@ -17,11 +20,27 @@ public class Controller  {
   public void initialize() {
     mytext.setText("Hello World from initialize!");
     mytextarea.setPrefRowCount(5);
+    init();
 
   }
   private void init() {
 
-
+    CongressHearing h1 =  new CongressHearing("Senate",
+      "Nomination-Kahl","Room SD-106, Dirksen Senate Office Building",
+      "Thursday, March 4, 2021. 09:30AM");
+      Class<?> c = h1.getClass();
+      Method[] allMethods = c.getDeclaredMethods();
+    for (Method m : allMethods) {
+      if (m.getName().startsWith("get") ) {
+        try {
+          mytextarea.appendText(m.invoke(h1).toString() + '\n');
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        } catch (InvocationTargetException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
 
 
